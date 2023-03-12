4a9e122c90453da1da797f392041e3ec13684286
csjoy <antohin.joy@gmail.com>
2023-03-12 19:39:56 +0600

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Svelte-Basics-
+
 Learned the base syntax, core features, conditionals, loops and reactivity.
+
+<img src="https://raw.githubusercontent.com/csjoy/Images/master/svelte.png" />
--- src/components/ContactCard.svelte
+++ src/components/ContactCard.svelte
@@ -73,7 +73,7 @@
   />
 
   <button
-    on:click|once={addContact}
+    on:click={addContact}
     class="mb-4 rounded-md bg-pink-500 px-5 py-2.5 font-medium text-white"
     >Add Contact Card</button
   >
